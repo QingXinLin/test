1f36a7375148b99a1e40ac18276fcd42a80c3dfd
QingXinLin <39868143+QingXinLin@users.noreply.github.com>
2018-06-03 09:43:37 +0800

    zuoye1
    今天是星期六

---
+++ day01/xinqi.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  3 08:50:02 2018
+
+@author: lenovo
+"""
+k=['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期天']
+if k[5]=='星期六' :
+    print('今天是'+k[5])   
+print(k[0])
+print(k[1])
+print(k[2])
+print(k[3]) 
+print(k[4])
+print(k[6])
+
